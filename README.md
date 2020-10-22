47cc3d221de543e3f2bea5178cc4ec2c93baf5ba
Eleftherios Kyriakakis <elky@dtu.dk>
2020-10-22 13:25:36 +0200

    Changed function call and removed optional arguments

--- README.md
+++ README.md
@@ -3,10 +3,22 @@
 An online simple schedule generator based on [SimpleSMTScheduler](https://github.com/egk696/SimpleSMTScheduler)
 To use the application online: [Simple Cyclic Scheduler Online](https://simplescheduleronline.herokuapp.com/)  
 ### Quick Start for local dev:
-Spin up the containers:
+Spin up the containers with:
+```bash
+docker-compose up -d --build -V
+```
+Then open your browser to http://localhost:5004
 
-```sh
-$ docker-compose up -d --build -V
+To check-up on your containers do:
+```bash
+docker ps
+```
+Read the logs of a container with:
+```bash
+docker logs [containerid]
+```
+Kill your containers:
+```bash
+docker stop $(docker ps -aq)
 ```
 
-Open your browser to http://localhost:5004
--- project/server/main/tasks.py
+++ project/server/main/tasks.py
@@ -1,34 +1,36 @@
 # from project.server.scheduling. import get_SMT_sched
-import simplejson as json
 import sys
+
+import simplejson as json
 
 sys.path.insert(0, '../SimpleSMTScheduler')
 from simplesmtscheduler.schedulers import *
 
 from io import StringIO
 
+
 def create_task(file, wcet_offset, optimize=False, simAnnealing=False):
     tasksFileName = file
     taskSet = []
     verbose = False
-    schedulePlotPeriods = 1
+    imgData = None
+    codeString = ""
 
     parse_csv_taskset(tasksFileName, taskSet)
 
     if simAnnealing is False:
-        schedule, utilization, hyperPeriod, elapsedTime = gen_cyclic_schedule_model(taskSet, wcet_offset, optimize, verbose)
+        schedule, utilization, hyperPeriod, elapsedTime = gen_cyclic_schedule_model(taskSet, wcet_offset, optimize,
+                                                                                    verbose)
 
     if schedule is not None:
         gen_schedule_activations(schedule, taskSet)
-
-    schedulePlot = plot_cyclic_schedule(taskSet, hyperPeriod, schedulePlotPeriods)
-    c_code = gen_schedule_code("", tasksFileName, taskSet, hyperPeriod, utilization, False)
-    imgBuf = StringIO()
-    schedulePlot.savefig(imgBuf, format="svg")
-    imgData = imgBuf.getvalue()
-
-    c_code.seek(0)
-    codeString = c_code.read()
+        c_code = gen_schedule_code("", tasksFileName, taskSet, hyperPeriod, utilization, False)
+        c_code.seek(0)
+        codeString = c_code.read()
+        schedulePlot = plot_cyclic_schedule(taskSet, hyperPeriod)
+        imgBuf = StringIO()
+        schedulePlot.savefig(imgBuf, format="svg")
+        imgData = imgBuf.getvalue()
 
     retVal = dict()
     retVal['img'] = f"{imgData}"
